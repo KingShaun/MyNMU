e28e2ce29ec4c48f34706330f5c570fa25ca406b
KingShaun <shaun@nmmu.ac.za>
2017-06-26 15:00:24 +0200

    Updated map zoom

--- platforms/browser/www/js/NMMUFunctions.js
+++ platforms/browser/www/js/NMMUFunctions.js
@@ -673,7 +673,7 @@ function onDeviceReady() {
         currentPosition = new google.maps.LatLng(lat, lon);
 
         map = new google.maps.Map(document.getElementById('map_canvas'), {
-            zoom: 15,
+            zoom: 8,
             center: currentPosition,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         });
